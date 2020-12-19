4764a52a025c1d55481fe3a61b3c626ddbc8c16d
yezii <1126yezy@gmail.com>
2020-12-19 14:17:08 +0900

    Footer and Auth UI

---
+++ src/Components/Footer.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: 12px;
+`;
+
+const List = styled.ul`
+  display: flex;
+`;
+
+const ListItem = styled.li`
+  &:not(:last-child) {
+    margin-right: 16px;
+  }
+`;
+
+const Link = styled.a`
+  color: ${(props) => props.theme.darkBlueColor};
+`;
+
+const Copyright = styled.span`
+  color: ${(props) => props.theme.darkGreyColor};
+`;
+
+export default () => (
+  <Footer>
+    <List>
+      <ListItem>
+        <Link href="#">ABOUT US</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">SUPPORT</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">PRESS</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">API</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">JOBS</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">PRIVACY</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">TERMS</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">DIRECTORY</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">PROFILES</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">HASHTAGS</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">LANGUAGE</Link>
+      </ListItem>
+    </List>
+    <Copyright>Prismagram {new Date().getFullYear()} &copy;</Copyright>
+  </Footer>
+);
--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -3,15 +3,25 @@ import reset from "styled-reset";
 
 export default createGlobalStyle`
     ${reset};
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700');
+    @import url("https://fonts.googleapis.com/css2?family=Lobster&display=swap");
     * {
         box-sizing: border-box;
     }
+
     body {
         background-color: ${(prop) => prop.theme.bgColor};
         color: ${(prop) => prop.theme.blackColor};
+        font-size:14px;
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
+
     a{
         color: ${(prop) => prop.theme.blueColor};
         text-decoration: none;
+    }
+
+    input:focus{
+        outline:none;
     }
 `;
